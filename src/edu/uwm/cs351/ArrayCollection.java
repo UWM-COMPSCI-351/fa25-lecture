8759c1aa9e63b009833ac01b066e9eb467852c86
boyland <boyland@UWM014851.ad.uwm.edu>
2025-10-09 09:52:26 -0500

    errors gone!

---
+++ src/edu/uwm/cs351/ArrayCollection.java
@@ -0,0 +1,48 @@
+package edu.uwm.cs351;
+
+import java.util.AbstractCollection;
+import java.util.Iterator;
+
+public class ArrayCollection<E> extends AbstractCollection<E> {
+	private static final int INITIAL_CAPACITY = 1;
+	
+	private E[] data;
+	private int size;
+	
+	private void ensureCapacity(int needed) {
+		if (needed <= data.length) return;
+		int newCap = needed;
+		if (newCap < data.length*2) newCap = data.length*2;
+		E[] newData = makeArray(newCap);
+		for (int i=0; i < size; ++i) {
+			newData[i] = data[i];
+		}
+		data = newData;
+	}
+
+	@SuppressWarnings("unchecked")
+	private E[] makeArray(int newCap) {
+		return (E[])new Object[newCap];
+	}
+	
+	public ArrayCollection() {
+		data = makeArray(INITIAL_CAPACITY);
+	}
+	
+	@Override // required
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override // required
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public E[] toArray() {
+		return data; // XXX very bad idea
+	}
+}
