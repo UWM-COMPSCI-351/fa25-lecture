3f82fa11b19106d4bdf0752b39d1024081753749
boyland <boyland@UWM014851.ad.uwm.edu>
2025-10-16 09:43:15 -0500

    fixed compiler errors

--- src/edu/uwm/cs351/Queue.java
+++ src/edu/uwm/cs351/Queue.java
@@ -2,4 +2,37 @@ package edu.uwm.cs351;
 
 public class Queue<E> {
 
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return -1;
+	}
+
+	public void enqueue(E elem) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Return the element at the front of the queue.
+	 * @return element at front, without removing it.
+	 */
+	public E front() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Remove and return the element at the front of the queye
+	 * @return former front element
+	 */
+	public E dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
