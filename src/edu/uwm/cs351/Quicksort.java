c69f083ee38111b96dd0dc52eed70b6f032a95aa
boyland <boyland@UWM014851.ad.uwm.edu>
2025-12-04 09:58:53 -0600

    wrote first draft of code

--- src/edu/uwm/cs351/Quicksort.java
+++ src/edu/uwm/cs351/Quicksort.java
@@ -1,7 +1,9 @@
 package edu.uwm.cs351;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.Iterator;
 
 public class Quicksort<T> {
 	private final Comparator<T> comp;
@@ -10,6 +12,22 @@ public class Quicksort<T> {
 	}
 	
 	public Collection<T> sort(Collection<T> input) {
-		return input;
+		Collection<T> bigger = new ArrayList<>();
+		Collection<T> smaller = new ArrayList<>();
+		Iterator<T> it = input.iterator();
+		T pivot = it.next();
+		while (it.hasNext()) {
+			T elem = it.next();
+			if (comp.compare(pivot, elem) > 0) {
+				smaller.add(elem);
+			} else {
+				bigger.add(elem);
+			}
+		}
+		bigger = sort(bigger);
+		smaller = sort(smaller);
+		smaller.add(pivot);
+		smaller.addAll(bigger);
+		return smaller;
 	}
 }
