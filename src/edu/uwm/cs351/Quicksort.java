a4aa20cfbd1340f9e6b9d9bd03063acf40a95ff6
boyland <boyland@UWM014851.ad.uwm.edu>
2025-12-04 09:37:50 -0600

    compiler errors fixed

---
+++ src/edu/uwm/cs351/Quicksort.java
@@ -0,0 +1,15 @@
+package edu.uwm.cs351;
+
+import java.util.Collection;
+import java.util.Comparator;
+
+public class Quicksort<T> {
+
+	public Quicksort(Comparator<T> c) {
+			
+	}
+	
+	public Collection<T> sort(Collection<T> input) {
+		return input;
+	}
+}
